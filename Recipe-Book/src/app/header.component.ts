31d1317ad5818c1583af15a9e0fee68cc76a5bfb
Simon Phillips <simon.phillips01@gmail.com>
2017-02-19 23:01:59 +0000

    Http added

--- Recipe-Book/src/app/header.component.ts
+++ Recipe-Book/src/app/header.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DropdownDirective } from './dropdown.directive';
+
+import { RecipeService } from './recipes/recipe.service';
 
 @Component({
   selector: 'rb-header',
   templateUrl: './header.component.html'.
   directives: [DropdownDirective]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {
+  onStore() {
+    this.recipeService.storeData().subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
   }
 
+  onFetch() {
+    this.recipeService.fetchData();
+  }
 }
